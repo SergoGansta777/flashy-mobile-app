a1f291604d2e1d81d6d368dcad778d1f504f0e3a
sergejnehorosev <sergo777ser777@gmail.com>
2024-11-14 12:04:00 +0300

    feat: implement onboarding welcome screen (without images)

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,12 +1,72 @@
-import React from "react";
-import { SafeAreaView, Text } from "react-native";
+import { Button } from "@/components/ui/button";
+import { onboarding } from "@/constants";
+import { router } from "expo-router";
+import React, { useMemo, useRef, useState } from "react";
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import Swiper from "react-native-swiper";
 
 const Welcome = () => {
+	const swiperRef = useRef<Swiper>(null);
+	const [activeIndex, setActiveIndex] = useState(0);
+
+	const isLastSlide = useMemo(
+		() => activeIndex === onboarding.length - 1,
+		[activeIndex],
+	);
 	return (
-		<SafeAreaView className="w-full h-full">
-			<Text className="flex items-center justify-center text-4xl text-red-600 text-center">
-				Welcome
-			</Text>
+		<SafeAreaView className="flex h-full items-center justify-between ">
+			<TouchableOpacity
+				onPress={() => {
+					router.replace("/(auth)/sign-up");
+				}}
+				className="w-full flex justify-end items-end p-5"
+			>
+				<Text className="text-black text-md font-JakartaBold">Skip</Text>
+			</TouchableOpacity>
+
+			<Swiper
+				ref={swiperRef}
+				loop={false}
+				dot={
+					<View className="w-[32px] h-[4px] mx-1 bg-gray-300 rounded-full" />
+				}
+				activeDot={
+					<View className="w-[32px] h-[4px] mx-1 bg-primary rounded-full" />
+				}
+				onIndexChanged={(index) => setActiveIndex(index)}
+			>
+				{onboarding.map((item) => (
+					<View key={item.id} className="flex items-center justify-center p-5">
+						{/* <Image
+							source={item.image}
+							className="w-full h-[300px]"
+							resizeMode="contain"
+						/> */}
+						<View className="flex flex-row items-center justify-center w-full mt-10">
+							<Text className="text-black text-3xl font-bold mx-10 text-center">
+								{item.title}
+							</Text>
+						</View>
+						<Text className="text-md font-JakartaSemiBold text-center text-slate-500 mx-10 mt-3">
+							{item.description}
+						</Text>
+					</View>
+				))}
+			</Swiper>
+
+			<Button
+				className="w-10/12"
+				size="lg"
+				onPress={() => {
+					isLastSlide
+						? router.replace("/(auth)/sign-up")
+						: swiperRef.current?.scrollBy(1);
+				}}
+			>
+				<Text className="text-white text-xl font-semibold">
+					{isLastSlide ? "Get Started" : "Next"}
+				</Text>
+			</Button>
 		</SafeAreaView>
 	);
 };
---
+++ constants/index.ts
@@ -0,0 +1,20 @@
+export const onboarding = [
+	{
+		id: 1,
+		title: 'Master Any Subject, One Card at a Time!',
+		description:
+			'Begin your learning journey by creating personalized flashcards and quizzes to fit your study goals.',
+	},
+	{
+		id: 2,
+		title: 'Learn Faster, Retain Better',
+		description:
+			'Use smart learning techniques like spaced repetition to ensure you remember what you study.',
+	},
+	{
+		id: 3,
+		title: "Ready to Ace Your Next Exam? Let's Go!",
+		description:
+			'Dive into your flashcard sets, track your progress, and get ready to achieve your learning targets!',
+	},
+]
