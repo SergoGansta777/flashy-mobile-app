54a925e1e98b7ee2aa4ac157492f1c5ed79dac6e
sergejnehorosev <sergo777ser777@gmail.com>
2024-12-28 07:54:32 +0300

    chore: revert commited by mistake line

--- components/deck-learn/empty-deck.tsx
+++ components/deck-learn/empty-deck.tsx
@@ -7,7 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context";
 const EmptyDeck = () => {
   return (
     <SafeAreaView className="relative flex h-full w-full flex-col items-center justify-start bg-background px-8">
-      <H3 className="mb">
+      <H3 className="mb-10 mt-7 w-full text-center text-3xl">
         It seems you haven&apos;t added any cards here yet.
       </H3>
 
